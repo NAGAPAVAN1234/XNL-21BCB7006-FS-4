fe5744ae0d88335072526c431dbd61d4b1206167
PavanGarikipati <145032497+NAGAPAVAN1234@users.noreply.github.com>
2025-03-16 08:14:16 +0530

    Update page.js

--- src/app/hire/page.js
+++ src/app/hire/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect, Suspense } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+
 import Image from 'next/image';
 import { StarIcon } from '@heroicons/react/24/solid';
 import { FiSearch, FiFilter, FiX } from 'react-icons/fi';
